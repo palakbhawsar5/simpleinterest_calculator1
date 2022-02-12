54178fa7104458d8c89f2325818546f56bbab328
palakbhawsar5 <94602436+palakbhawsar5@users.noreply.github.com>
2022-02-12 12:42:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function rateValue(val){
+	document.getElementById('rateInput').value=val;
+
+}
+
+
+function calculate(){
+	
+amount = document.getElementById('amount').value;
+rateInput= document.getElementById('rateInput').value;
+selectId=document.getElementById('selectId').value;
+resultat=document.getElementById('resultat');
+
+var today= new Date();
+var annee = today.getFullYear();
+var result1=((annee=+selectId));
+var today2=new Date();
+var annee2 =today2.getFullYear();
+var result2=result1+annee2
+
+/* calul et affichage de l'intérêt*/
+resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
+"in the year"+"&nbsp;"+(result2);
+}
